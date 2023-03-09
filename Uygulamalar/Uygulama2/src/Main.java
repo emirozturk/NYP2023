b6e2a3991ee15c64bd9f9413f77c0c8a7c5663b9
emirozturk <emirozturk3@gmail.com>
2023-03-09 13:09:24 +0300

    Son hali

--- Uygulamalar/Uygulama2/src/Main.java
+++ Uygulamalar/Uygulama2/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -8,27 +10,55 @@ public class Main {
         return "Çok";
     }
     public static void main(String[] args) {
+        var bmiListesi = new ArrayList<String>();
         var scanner = new Scanner(System.in);
         int sayac=0;
+        float boy,kilo;
         while(sayac<3){
+            /*
+            try{
+                System.out.println((sayac+1)+". kişi:");
+                System.out.println("Boy giriniz (m):");
+                var stringBoy = scanner.nextLine();
+                stringBoy = stringBoy.replace(",",".");
+                boy = Float.parseFloat(stringBoy);
+                if(boy<0){
+                    System.out.println("Yanlış boy değeri");
+                    continue;
+                }
+                System.out.println("Ağırlık giriniz (kg)");
+                kilo = Float.parseFloat(scanner.nextLine().replace(",","."));
+                if(kilo<0){
+                    System.out.println("Yanlış kilo değeri");
+                    continue;
+                }
+            }
+            catch (Exception ex){
+                System.out.println("Hatalı değer");
+                continue;
+            }
+            */
             System.out.println((sayac+1)+". kişi:");
             System.out.println("Boy giriniz (m):");
-            var stringBoy = scanner.nextLine();
-            stringBoy = stringBoy.replace(",",".");
-            var boy = Float.parseFloat(stringBoy);
-            if(boy<0){
-                System.out.println("Yanlış boy değeri");
-                continue;
-            }
+            boy = scanner.nextFloat();
             System.out.println("Ağırlık giriniz (kg)");
-            var kilo = Float.parseFloat(scanner.nextLine().replace(",","."));
-            if(kilo<0){
-                System.out.println("Yanlış kilo değeri");
-                continue;
-            }
+            kilo = scanner.nextFloat();
             var sonuc = bmiHesapla(boy,kilo);
+            bmiListesi.add(sonuc);
             System.out.println(sonuc);
             sayac++;
+        }
+        int[] sayaclar = new int[3];
+        var etiketler = Arrays.asList("Düşük","Normal","Çok");
+        for(var eleman:bmiListesi){
+            switch (eleman) {
+                case "Düşük" -> sayaclar[0]++;
+                case "Normal" -> sayaclar[1]++;
+                case "Çok" -> sayaclar[2]++;
+            }
+        }
+        for(int i=0;i<sayaclar.length;i++){
+            System.out.println(etiketler.get(i)+" grubunda "+ sayaclar[i]+" kişi var.");
         }
     }
 }
