fddfcfd76842101059d30aee929eb65e71614331
emirozturk <emirozturk3@gmail.com>
2023-03-09 12:43:50 +0300

    Update Main.java

--- Uygulamalar/Uygulama2/src/Main.java
+++ Uygulamalar/Uygulama2/src/Main.java
@@ -1,23 +1,24 @@
 import java.util.Scanner;
 
 public class Main {
+    static String bmiHesapla(float boy,float kilo){
+        var bmi = kilo / Math.pow(boy,2);
+        if(bmi < 18.5) return "Düşük";
+        else if(bmi<24.9) return "Normal";
+        return "Çok";
+    }
     public static void main(String[] args) {
         var scanner = new Scanner(System.in);
-        System.out.println("Boy giriniz (m):");
-        var stringBoy = scanner.nextLine();
-        stringBoy = stringBoy.replace(",",".");
-        var boy = Float.parseFloat(stringBoy);
-        System.out.println("Ağırlık giriniz (kg)");
-        var kilo = Float.parseFloat(scanner.nextLine().replace(",","."));
-        var bmi = kilo / Math.pow(boy,2);
-        if(bmi < 18.5){
-            System.out.println("Düşük");
-        }
-        else if(bmi<24.9){
-            System.out.println("Normal");
-        }
-        else{
-            System.out.println("Çok");
+        for(int i=0;i<3;i++){
+            System.out.println((i+1)+". kişi:");
+            System.out.println("Boy giriniz (m):");
+            var stringBoy = scanner.nextLine();
+            stringBoy = stringBoy.replace(",",".");
+            var boy = Float.parseFloat(stringBoy);
+            System.out.println("Ağırlık giriniz (kg)");
+            var kilo = Float.parseFloat(scanner.nextLine().replace(",","."));
+            var sonuc = bmiHesapla(boy,kilo);
+            System.out.println(sonuc);
         }
     }
 }
