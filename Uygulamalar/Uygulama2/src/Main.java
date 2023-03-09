4ed8c61afeec379e1dae7424181389ccb118a2c3
emirozturk <emirozturk3@gmail.com>
2023-03-09 11:55:24 +0300

    Update Main.java

--- Uygulamalar/Uygulama2/src/Main.java
+++ Uygulamalar/Uygulama2/src/Main.java
@@ -2,12 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        var degisken = 98234;
-        System.out.println(degisken);
-        var scanner = new Scanner(System.in);
-        System.out.println("Bir sayı giriniz:");
-        var ekrandanAlinan = scanner.nextInt();
-        degisken = ekrandanAlinan;
-        System.out.println(degisken);
+        //Ekrandan boy değerini al (m)
+        //Ekrandan kilo değerini al (kg)
+        //bmi değerini kilo / boy * boy olarak hesapla
+        //bu değeri kontrol et
+        //Eğer 18.5 altındaysa
+            //Düşük
+        //Eğer 18.5 ve 24.9 arasındaysa
+            //Normal
+        //Eğer 25 ve üstü ise
+            //Çok
+        
     }
 }
