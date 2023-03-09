f4de38efee8aa03b91284a984913985c4fa2eafc
emirozturk <emirozturk3@gmail.com>
2023-03-09 12:32:08 +0300

    Update Main.java

--- Uygulamalar/Uygulama2/src/Main.java
+++ Uygulamalar/Uygulama2/src/Main.java
@@ -4,9 +4,11 @@ public class Main {
     public static void main(String[] args) {
         var scanner = new Scanner(System.in);
         System.out.println("Boy giriniz (m):");
-        var boy = scanner.nextFloat();
+        var stringBoy = scanner.nextLine();
+        stringBoy = stringBoy.replace(",",".");
+        var boy = Float.parseFloat(stringBoy);
         System.out.println("Ağırlık giriniz (kg)");
-        var kilo = scanner.nextFloat();
+        var kilo = Float.parseFloat(scanner.nextLine().replace(",","."));
         var bmi = kilo / Math.pow(boy,2);
         if(bmi < 18.5){
             System.out.println("Düşük");
