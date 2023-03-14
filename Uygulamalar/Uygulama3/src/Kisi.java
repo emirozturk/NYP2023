ec7f21ad89f24b17ff1e5201a2cf70efc8d88551
emirozturk <emirozturk3@gmail.com>
2023-03-14 13:23:21 +0300

    Uygulama 3'ün eksik hali
    Hatalı bir şekilde yazım başlatıldı. Perşembe günü düzeltmeler yapılarak nesneye yönelik hale getirilecek

---
+++ Uygulamalar/Uygulama3/src/Kisi.java
@@ -0,0 +1,10 @@
+class Kisi{
+    private String ad;
+    int yas;
+    public void setAd(String ad){
+        this.ad = ad;
+    }
+    public String getAd(){
+        return ad;
+    }
+}
---
+++ Uygulamalar/Uygulama3/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Kisi yeniKisi = new Kisi();
+        yeniKisi.setAd("Emir");
+        yeniKisi.yas = 300;
+        System.out.println(yeniKisi.getAd() + " " + yeniKisi.yas);
+    }
+}
