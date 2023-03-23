851ccd70c53af82297c9d6574558db561535834d
emirozturk <emirozturk3@gmail.com>
2023-03-23 12:09:38 +0300

    Update Main.java

--- Uygulamalar/Uygulama4_1/src/Main.java
+++ Uygulamalar/Uygulama4_1/src/Main.java
@@ -1,11 +1,27 @@
+class Kutu{
+    private int en;
+    private int boy;
+    private int yukseklik;
+    public Kutu(int en,int boy,int yukseklik){
+        this.en = en;
+        this.boy = boy;
+        this.yukseklik = yukseklik;
+    }
+    public void setEn(int en){
+        this.en = en;
+    }
+    public int getEn(){
+        return en;
+    }
+}
 public class Main {
-    public static int fonksiyon(int deger){
-        deger = 100;
-        return deger;
+    public static void fonksiyon(Kutu degisken){
+        //degisken = new Kutu(5,5,5);
+        degisken.setEn(10);
     }
     public static void main(String[] args) {
-        int deger = 10;
-        deger = fonksiyon(deger);
-        System.out.println(deger);
+        Kutu k = new Kutu(3,3,3);
+        fonksiyon(k);
+        System.out.println(k.getEn());
     }
 }
