d420b65c87a313270845835dc1f11473d11d3c1f
emirozturk <emirozturk3@gmail.com>
2023-03-23 11:43:22 +0300

    Uygulama 4.1 1. adım

---
+++ Uygulamalar/Uygulama4_1/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    static byte birByte;
+    static int tamsayi;
+    static long buyukTamsayi;
+    static float ondalikliSayi;
+    static boolean evetYaDaHayir;
+    static String metin;
+
+    public static void main(String[] args) {
+        System.out.println(tamsayi);
+        System.out.println(buyukTamsayi);
+        System.out.println(ondalikliSayi);
+        System.out.println(evetYaDaHayir);
+        System.out.println(metin);
+    }
+}
