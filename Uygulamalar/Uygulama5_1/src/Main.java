37d5a38296be8338282369ade633e2d4aeb49a4a
emirozturk <emirozturk3@gmail.com>
2023-03-30 12:12:43 +0300

    Update Main.java

--- Uygulamalar/Uygulama5_1/src/Main.java
+++ Uygulamalar/Uygulama5_1/src/Main.java
@@ -1,5 +1,40 @@
+class Sinif{
+    int a;
+    int b;
+    int c;
+    public Sinif(int a,int b,int c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    public Sinif(int a,int b){
+        this.a = a;
+        this.b = b;
+        c = -1;
+    }
+    public Sinif(int a){
+        this.a = a;
+        b = -1;
+        c = -1;
+    }
+    public Sinif(){
+
+    }
+    public String toString(){
+        return String.format("%d %d %d",a,b,c);
+    }
+}
 public class Main {
     public static void main(String[] args) {
+        var nesne1 = new Sinif();
+        System.out.println(nesne1);
+        var nesne2 = new Sinif(1);
+        System.out.println(nesne2);
+        var nesne3 = new Sinif(1,2);
+        System.out.println(nesne3);
+        var nesne4 = new Sinif(1,2,3);
+        System.out.println(nesne4);
+
         var yazdirma = new Yazdirma();
         yazdirma.yazdir(13487);
         yazdirma.yazdir(183.439f);
